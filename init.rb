5a343dc7cfc4b8ef546e1026f840b6d9c77aae29
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2022-03-18 23:58:56 +0900

    バージョンを 0.2.0 に更新した。

--- init.rb
+++ init.rb
@@ -7,7 +7,7 @@ Redmine::Plugin.register :redmine_wiki_diff_alternative do
   name 'Redmine Wiki Diff Alternative plugin'
   author '9506hqwy'
   description 'This is a wiki diff alternative view plugin for Redmine'
-  version '0.1.0'
+  version '0.2.0'
   url 'https://github.com/9506hqwy/redmine_wiki_diff_alternative'
   author_url 'https://github.com/9506hqwy'
 
